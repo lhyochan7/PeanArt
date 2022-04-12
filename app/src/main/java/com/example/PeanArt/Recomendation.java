d44c69426ac7314e19beaeefe24524bf5844e04c
kids9702 <kamji9702@naver.com>
2022-04-12 17:53:51 +0900

    0412-5

--- app/src/main/java/com/example/PeanArt/Recomendation.java
+++ app/src/main/java/com/example/PeanArt/Recomendation.java
@@ -3,16 +3,32 @@ package com.example.PeanArt;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Recomendation extends AppCompatActivity {
+
+    // Member Variable
+
+    ImageView venusIMG, screamIMG, starryIMG, monarisaIMG, pearlIMG, kissIMG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recomendation);
+
+        venusIMG = (ImageView) findViewById(R.id.venusIMG);
+        screamIMG = (ImageView) findViewById(R.id.screamIMG);
+        starryIMG = (ImageView) findViewById(R.id.starryIMG);
+        monarisaIMG = (ImageView) findViewById(R.id.monarisaIMG);
+        pearlIMG = (ImageView) findViewById(R.id.pearlIMG);
+        kissIMG = (ImageView) findViewById(R.id.kissIMG);
+
     }
 
     public void rcmd(View view) {
+        switch (view.getId()){
+            case R.id.venusIMG:
 
+        }
     }
 }
