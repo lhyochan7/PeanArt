9b6ddb6c0f211cb764a69a7d7212576507af5617
lhyochan7 <lhyochan7@gmail.com>
2022-04-12 17:49:06 +0900

    Added on 2022/04/12

--- app/src/main/java/com/example/PeanArt/RegisterGallery.java
+++ app/src/main/java/com/example/PeanArt/RegisterGallery.java
@@ -3,18 +3,12 @@ package com.example.PeanArt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.WindowManager;
 
-public class MainPage extends AppCompatActivity {
+public class RegisterGallery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //setContentView(R.layout.activity_main);
-
         setContentView(R.layout.activity_register_gallery);
     }
-
 }
