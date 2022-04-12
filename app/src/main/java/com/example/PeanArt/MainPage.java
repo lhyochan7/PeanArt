663aeef5903df27d5c1722ef5a762c192f22f5a2
lhyochan7 <lhyochan7@gmail.com>
2022-04-12 15:25:20 +0900

    Hi

--- app/src/main/java/com/example/PeanArt/MainPage.java
+++ app/src/main/java/com/example/PeanArt/MainPage.java
@@ -3,12 +3,16 @@ package com.example.PeanArt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
 }
