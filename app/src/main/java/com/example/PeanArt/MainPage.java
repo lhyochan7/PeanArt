8cf2290a5b8582a2317c56166f77ac223af00e02
cloudsoswift <magic9282m@gmail.com>
2022-04-13 17:58:44 +0900

    jeongri

--- app/src/main/java/com/example/PeanArt/MainPage.java
+++ app/src/main/java/com/example/PeanArt/MainPage.java
@@ -1,29 +1,34 @@
 package com.example.PeanArt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
-    BottomNavigationMenuView bottomNavigationMenuView;
-
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    FragmentTransaction fragmentTransaction;
-
-    detailPageFragment detailPageFragment = new detailPageFragment();
-    myPageFragment myPageFragment = new myPageFragment();
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.underbar);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                }
+                return true;
+            }
+        });
     }
 
 }
