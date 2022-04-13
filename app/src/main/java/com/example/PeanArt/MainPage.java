38aec9354f93d9010898c4fe4cdf7c5c42d034a8
cloudsoswift <magic9282m@gmail.com>
2022-04-13 10:33:11 +0900

    hadan bar man deum

--- app/src/main/java/com/example/PeanArt/MainPage.java
+++ app/src/main/java/com/example/PeanArt/MainPage.java
@@ -1,11 +1,22 @@
 package com.example.PeanArt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+
 public class MainPage extends AppCompatActivity {
+    BottomNavigationMenuView bottomNavigationMenuView;
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction;
+
+    detailPageFragment detailPageFragment = new detailPageFragment();
+    myPageFragment myPageFragment = new myPageFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
