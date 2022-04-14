d645ad80570dabb7e0cfe7cbf6e50989e28535c4
kids9702 <kamji9702@naver.com>
2022-04-14 16:49:37 +0900

    0413-2

--- app/src/main/java/com/example/PeanArt/MainPage.java
+++ app/src/main/java/com/example/PeanArt/MainPage.java
@@ -5,60 +5,30 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.lang.reflect.Member;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends Fragment {
 
     private String TAG = "MainPage";
 
-    private Button registerBTN;
-
-    // Feature
-    private ActivityResultLauncher<Intent> mLauncher;
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_main, container, false);
 
-        registerBTN = (Button) findViewById(R.id.registerBTN);
-
-
-        // (3) 데이터 준비
-        mLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>(){
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if(result.getResultCode()==RESULT_OK) {
-                            // 결과 + 데이터가 온 경우 => Intent 객체를 꺼내기
-                            Intent data = result.getData();
-                            Toast.makeText(MainPage.this, "RECEIVE DATA : ", Toast.LENGTH_SHORT).show();
-
-                        } else if(result.getResultCode()==RESULT_CANCELED) {
-                            Log.i(TAG, "CANCELED");
-                        }
-                    }
-                });
+        return rootView;
     }
-
-
-    public void clickFunc(View view) {
-        switch(view.getId()) {
-            case R.id.registerBTN:
-                mLauncher.launch(new Intent(this, RegisterGallery.class));
-                break;
-        }
-
-    }
-
 }
