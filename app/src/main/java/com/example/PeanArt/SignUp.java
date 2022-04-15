dfdd229c8e08146fc91a4524642ff850e845bedb
sangho <hn01184@gmail.com>
2022-04-15 11:38:37 +0900

    sangho 0412

--- app/src/main/java/com/example/PeanArt/SignUp.java
+++ app/src/main/java/com/example/PeanArt/SignUp.java
@@ -1,17 +1,29 @@
 package com.example.PeanArt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
     // Member Variable
     Button signBTN;
     EditText usernameETXT, nicknameETXT, pwdETXT, emailETXT;
+    private FirebaseAuth mAuth;
+    private String TAG = "SignUp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,21 +35,57 @@ public class SignUp extends AppCompatActivity {
         pwdETXT = (EditText) findViewById(R.id.pwdETXT);
         emailETXT = (EditText) findViewById(R.id.emailETXT);
 
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser !=null){ //sangho
+            //pass
+        }else{
+            Log.i(TAG, "이미 로그인 했습니다");
+        }
     }
 
     public void sign(View view) {
         Intent sign = new Intent(this, Login.class);
-        String user = usernameETXT.getText().toString();
+
         String nick = usernameETXT.getText().toString();
-        String pwd = usernameETXT.getText().toString();
-        String email = usernameETXT.getText().toString();
+        String pwd = pwdETXT.getText().toString();
+        String email = emailETXT.getText().toString().trim();
 
-        sign.putExtra("user", user);
-        sign.putExtra("nick", nick);
-        sign.putExtra("pwd", pwd);
-        sign.putExtra("email", email);
+        mAuth.createUserWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(SignUp.this, "Authentication success !!",
+                                    Toast.LENGTH_SHORT).show();
 
-        setResult(RESULT_OK, sign);
-        finish();
+                            sign.putExtra("userinfo", user);
+                            setResult(RESULT_OK, sign);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            setResult(RESULT_CANCELED, sign);
+                            finish();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+    private void updateUI(FirebaseUser user) {
+        //pass
     }
 }
