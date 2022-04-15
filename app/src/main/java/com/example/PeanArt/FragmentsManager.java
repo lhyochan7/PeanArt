98cc02349d12ce44f763724e3960373e64aee73b
kids9702 <kamji9702@naver.com>
2022-04-15 16:57:04 +0900

    0415

--- app/src/main/java/com/example/PeanArt/FragmentsManager.java
+++ app/src/main/java/com/example/PeanArt/FragmentsManager.java
@@ -1,6 +1,9 @@
 package com.example.PeanArt;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -8,15 +11,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.ktx.Firebase;
 
-public class FragmentsManager extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class FragmentsManager<var> extends AppCompatActivity {
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
     private MainPage fragmentMain = new MainPage();
     private Recomendation fragmentRecommend = new Recomendation();
     private LikeList fragmentLike = new LikeList();
     private MyPage fragmentMypage = new MyPage();
+
+    private final String TAG = "FragManager";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +52,84 @@ public class FragmentsManager extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        String uid = null;
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            uid = user.getUid();
+            Log.i(TAG, "FIRESTORE DATA : " + name);
+        }
+
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "test");
+//        user.put("second", "TEST");
+//        db.collection("user")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+////데이터가 성공적으로 추가되었을 때
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+////에러가 발생했을 때
+//                        Log.w(TAG, "Error ", e);
+//                    }
+//                });
+//
+
+        DocumentReference docRef = db.collection("user").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        Map<String, Object> userinfo = new HashMap<>();
+        userinfo.put("first", "test");
+
+        docRef
+                .set(userinfo, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "write successwrite successwrite successwrite successwrite successwrite successwrite successwrite success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "write failurewrite failurewrite failurewrite failurewrite failurewrite failurewrite failurewrite failurewrite failure");
+                    }
+                });
 
     }
 
