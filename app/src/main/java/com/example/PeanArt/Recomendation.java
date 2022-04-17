f825c8a25bd901701083830542cac08e928b13d2
kids9702 <kamji9702@naver.com>
2022-04-18 08:23:07 +0900

    0415-3

--- app/src/main/java/com/example/PeanArt/Recomendation.java
+++ app/src/main/java/com/example/PeanArt/Recomendation.java
@@ -1,10 +1,12 @@
 package com.example.PeanArt;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,11 @@ import android.widget.ImageView;
 
 public class Recomendation extends Fragment {
 
+    private final String TAG = "Recommendation";
+
     // Member Variable
+
+    String uid;
 
 //    ImageView venusIMG, screamIMG, starryIMG, monarisaIMG, pearlIMG, kissIMG;
 
@@ -29,6 +35,20 @@ public class Recomendation extends Fragment {
 //        kissIMG = (ImageView) findViewById(R.id.kissIMG);
 
         return rootView;
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            uid = bundle.getString("uid");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "RESUME UID : " + uid);
     }
 
 //    public void rcmd(View view) {
