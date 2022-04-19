4a4ce9a5317172442f0d0ba5986dfbb754d18130
cloudsoswift <magic9282m@gmail.com>
2022-04-19 19:02:36 +0900

    updating myPage..

--- app/src/main/java/com/example/PeanArt/MyPage.java
+++ app/src/main/java/com/example/PeanArt/MyPage.java
@@ -7,27 +7,46 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.PeanArt.adapter.ExhibitAdapter;
+import com.example.PeanArt.adapter.followedUserAdapter;
 import com.example.PeanArt.model.Exhibition;
+import com.example.PeanArt.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyPage extends Fragment {
 
     private final String TAG = "MyPage";
 
+    private FirebaseFirestore fs;
     String uid;
+    // User 프로필 사진 및 아이디 표시용 view
+    private ImageView profileImg;
+    private TextView welcome_name;
+    // Follow User Recycler View 용 Variable
+    ArrayList<User> mUserList;
+    RecyclerView rcView_follow;
+    followedUserAdapter mFollowAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,15 +55,83 @@ public class MyPage extends Fragment {
         if(bundle != null){
             uid = bundle.getString("uid");
         }
+        uid = "FTScRKFyelcxtPA2u2hN7bA7bJD3";
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_my_page, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_my_page, container, false);
+        fs = FirebaseFirestore.getInstance();
+        profileImg = rootView.findViewById(R.id.mypage_profileImg);
+        welcome_name = rootView.findViewById(R.id.mypage_welcome_name);
+
+        LoadUserData();
+        LoadFollowedUser();
         return rootView;
     }
+    public void LoadUserData(){
+        fs.collection("test_users").whereEqualTo(FieldPath.documentId(), uid).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for (DocumentSnapshot document : task.getResult()){
+                        if(document.exists()){
+                            Log.i(TAG, document.get("nickname").toString());
+                            User tmp = document.toObject(User.class);
+                            tmp.setID(document.getId());
+                            Log.i(TAG, "onComplete: "+ tmp.getID());
+                            SetProfileImg(tmp);
+                            welcome_name.setText(tmp.getNickname()+" 님 반갑습니다!");
+                        }
+                    }
+                } else{
+                    Log.e(TAG, "onComplete: ", task.getException());
+                }
+            }
+        });
+    }
+    public void LoadFollowedUser(){
+        fs.collection("test_users").whereEqualTo(FieldPath.documentId(), uid).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            //TODO: follow 안에 들어있는 userUID 와 documentID 같은놈들 찾기
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(DocumentSnapshot document : task.getResult()){
+                        if(document.exists()){
+                            Log.i(TAG, "onComplete: " + ((List<String>)document.get("follow")).toString());
+                            fs.collection("test_users").whereIn(FieldPath.documentId(), ((List<String>)document.get("follow"))).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if(task.isSuccessful()){
+                                        Log.i(TAG, "onComplete: " + task.getResult().getDocuments().toString());
+                                        for(DocumentSnapshot documentSnapshot : task.getResult()){
+                                            if(documentSnapshot.exists()){
+                                                Log.i(TAG, "found by Array: " + documentSnapshot.get("nickname").toString());
+                                            }
+                                        }
+                                    }else{}
+                                }
+                            });
+                        }
+                    }
+                }else {
 
+                }
+            }
+        });
+    }
+    public void SetProfileImg(User user){
+        StorageReference storageRef = FirebaseStorage.getInstance("gs://peanart-b433a.appspot.com/").getReference();
+        storageRef.child("User/"+user.getID()+".png").getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()){
+                    Glide.with(getView()).load(task.getResult()).into(profileImg);
+                }
+            }
+        });
+    }
     @Override
     public void onResume() {
         super.onResume();
--- app/src/main/java/com/example/PeanArt/adapter/followedUserAdapter.java
+++ app/src/main/java/com/example/PeanArt/adapter/followedUserAdapter.java
@@ -1,5 +1,6 @@
 package com.example.PeanArt.adapter;
 
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,18 +12,24 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.bumptech.glide.Glide;
 import com.example.PeanArt.R;
 import com.example.PeanArt.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class followedUserAdapter extends RecyclerView.Adapter<followedUserAdapter.ViewHolder> {
     private ArrayList<User> mUserList;
-
+    private StorageReference storageRef;
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+        storageRef = FirebaseStorage.getInstance("gs://peanart-b433a.appspot.com/").getReference();
         return new ViewHolder(view);
     }
 
@@ -48,13 +55,20 @@ public class followedUserAdapter extends RecyclerView.Adapter<followedUserAdapte
         TextView txtView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            // imageView = itemView.findViewById(R.id.followed_user_img);
-            // txtView = itemView.findViewById(R.id.followed_uesr_nickname);
+            imageView = itemView.findViewById(R.id.followed_user_img);
+            txtView = itemView.findViewById(R.id.followed_uesr_nickname);
         }
         void onBind(User user){
             // 추후 User 클래스를 parameter로 받는걸로 바꿔서 그 User에서
             // profile image, nickname 등을 get 해와야 함.
+            storageRef.child("User/"+user.getID()+".png").getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Glide.with(itemView).load(task.getResult()).into(imageView);
+                    }
+                }
+            });
             txtView.setText("닉네임: "+ user.getNickname());
         }
     }
