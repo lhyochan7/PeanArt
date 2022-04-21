080df86061c6731f8728876bb541dfc4494969c1
sangho <hn01184@gmail.com>
2022-04-21 17:49:31 +0900

    sangho 04/21 사진다중등록 version

--- app/src/main/java/com/example/PeanArt/Map.java
+++ app/src/main/java/com/example/PeanArt/Map.java
@@ -3,7 +3,6 @@ package com.example.PeanArt;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +10,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
